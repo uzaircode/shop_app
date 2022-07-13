ebd72d6f96701daf0a355c1f3b68f5682e438eec
Nik Uzair <nikuzairsc@gmail.com>
2022-07-14 07:59:57 +0800

    add popupMenuButton

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/cart.dart';
+import '../widgets/badge.dart';
 import '../widgets/products_grid.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favourites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavourite = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (selectedValue) {
+              //UI rebuilt when data change
+              setState(() {
+                if (selectedValue == FilterOptions.Favourites) {
+                  _showOnlyFavourite = true;
+                } else {
+                  _showOnlyFavourite = false;
+                }
+              });
+            },
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                  value: FilterOptions.Favourites,
+                  child: Text('Only Favourite')),
+              const PopupMenuItem(
+                  value: FilterOptions.All, child: Text('Show All')),
+              const PopupMenuItem(
+                  value: FilterOptions.Favourites,
+                  child: Text('Selected Favourite'))
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              value: cart.itemCount.toString(),
+              //the icon will not rebuild
+              child: ch,
+            ),
+            //define outside builder
+            child: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {
+              },
+            ),
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavourite),
     );
   }
 }
